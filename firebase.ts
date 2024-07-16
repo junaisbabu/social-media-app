bb7df12ccf7b01b624d755c5886b42e37b72d19a
Junais Babu <junaisdvs@gmail.com>
2024-07-16 23:56:22 +0530

    update firebase config

--- firebase.ts
+++ firebase.ts
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -17,5 +18,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = getApps.length === 0 ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(app);
+auth.useDeviceLanguage();
+
 const analytics = getAnalytics(app);
+
+export {auth};
