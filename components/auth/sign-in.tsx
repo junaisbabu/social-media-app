f517f2ea90bc33129a6fcd06602e68284058f182
Junais Babu <junaisdvs@gmail.com>
2024-07-19 23:58:48 +0530

    create sign in page ui

---
+++ components/auth/sign-in.tsx
@@ -0,0 +1,178 @@
+"use client";
+
+import React, { FormEvent, useEffect, useState, useTransition } from "react";
+import {
+  RecaptchaVerifier,
+  ConfirmationResult,
+  signInWithPhoneNumber,
+} from "firebase/auth";
+import { useRouter } from "next/navigation";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "../ui/input-otp";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { auth } from "@/firebase";
+
+function SignIn() {
+  const router = useRouter();
+
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [otp, setOtp] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [resendCountdown, setResendCountdown] = useState(0);
+
+  const [recaptchaVerifier, setRecaptchaVerifier] =
+    useState<RecaptchaVerifier | null>(null);
+
+  const [confirmationResult, setConformationResult] =
+    useState<ConfirmationResult | null>(null);
+
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (resendCountdown > 0) {
+      timer = setTimeout(() => setResendCountdown(resendCountdown - 1), 1000);
+    }
+
+    return () => clearTimeout(timer);
+  }, [resendCountdown]);
+
+  useEffect(() => {
+    const recaptchaVerifier = new RecaptchaVerifier(
+      auth,
+      "recaptcha-container",
+      { size: "invisible" }
+    );
+
+    setRecaptchaVerifier(recaptchaVerifier);
+
+    return () => recaptchaVerifier.clear();
+  }, [auth]);
+
+  useEffect(() => {
+    const hasEnteredAllDigits = otp.length === 6;
+    if (hasEnteredAllDigits) {
+      verifyOtp();
+    }
+  }, [otp]);
+
+  const verifyOtp = async () => {
+    startTransition(async () => {
+      setError("");
+
+      if (!confirmationResult) {
+        setError("Please request OTP first.");
+        return;
+      }
+
+      try {
+        await confirmationResult?.confirm(otp);
+        router.replace("/");
+      } catch (error) {
+        console.log(error);
+        setError("Failed to verify OTP. Please check the OTP.");
+      }
+    });
+  };
+
+  const requestOtp = async (e?: FormEvent<HTMLFormElement>) => {
+    e?.preventDefault();
+
+    setResendCountdown(60);
+
+    startTransition(async () => {
+      setError("");
+
+      if (!recaptchaVerifier) {
+        return setError("RecaptchaVerifier is not initialized.");
+      }
+
+      try {
+        const confirmationResult = await signInWithPhoneNumber(
+          auth,
+          phoneNumber,
+          recaptchaVerifier
+        );
+        setConformationResult(confirmationResult);
+        setSuccess("OTP sent successfully.");
+      } catch (err: any) {
+        console.log(err);
+        setResendCountdown(0);
+
+        if (err.code === "auth/inva fid-phone-number") {
+          setError("Invalid phone number. Please check the number.");
+        } else if (err.code === "auth/too-many-requests") {
+          setError("Too many requests. Please try again later.");
+        } else {
+          setError("Failed to send OTP. Please try again.");
+        }
+      }
+    });
+  };
+
+  return (
+    <div>
+      {!confirmationResult && (
+        <form className="w-full space-y-6">
+          <h1 className="text-2xl">Hi, Welcome Back! 👋</h1>
+          <div>
+            <Input
+              className="• text-black"
+              type="tel"
+              placeholder="+91"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+            <p className="text-xs text-gray-400 mt-2">
+              Please enter your number with the country code (i.e. +91 for IN){" "}
+            </p>
+          </div>
+        </form>
+      )}
+
+      {confirmationResult && (
+        <InputOTP maxLength={6} value={otp} onChange={(value) => setOtp(value)}>
+          <InputOTPGroup>
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+            <InputOTPSlot index={2} />
+          </InputOTPGroup>
+          <InputOTPSeparator />
+          <InputOTPGroup>
+            <InputOTPSlot index={3} />
+            <InputOTPSlot index={4} />
+            <InputOTPSlot index={5} />
+          </InputOTPGroup>
+        </InputOTP>
+      )}
+
+      <Button
+        disabled={!phoneNumber || isPending || resendCountdown > 0}
+        onClick={() => requestOtp()}
+        className="mt-5"
+      >
+        {resendCountdown > 0
+          ? `Resend OTP in ${resendCountdown}`
+          : isPending
+          ? "Sending OTP"
+          : "Send OTP"}
+      </Button>
+
+      <div className="p-10 text-center">
+        {error && <p className="text-red-500">{error}</p>}
+        {success && <p className="text-green-500">{success}</p>}
+      </div>
+
+      <div id="recaptcha-container" />
+
+      {isPending && "Loading..."}
+    </div>
+  );
+}
+export default SignIn;
