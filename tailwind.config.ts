5014241e218da0a719191f2268c3a6203e474326
Junais Babu <junaisdvs@gmail.com>
2024-08-01 23:06:31 +0530

    add default base font size as '14px'

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      fontSize: {
+        base: "14px",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
