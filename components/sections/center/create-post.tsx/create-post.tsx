20f63b6afc6e8c9077acac070b2a87986284c570
Junais Babu <junaisdvs@gmail.com>
2024-08-02 22:01:01 +0530

    create create post ui

---
+++ components/sections/center/create-post.tsx/create-post.tsx
@@ -0,0 +1,39 @@
+import { useAuthStore } from "@/components/auth/auth-state";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import React from "react";
+import { ImagePlus } from "lucide-react";
+
+function CreatePost() {
+  const { user } = useAuthStore();
+
+  return (
+    <div className="p-4 bg-white rounded-lg space-y-4">
+      <div className="flex items-center gap-2">
+        <Avatar className="w-8 h-8 rounded-xl">
+          <AvatarImage
+            src={user?.photoURL || "https://github.com/shadcn.png"}
+            alt={user?.displayName || "Anonymous"}
+          />
+        </Avatar>
+        <div className="relative flex-1">
+          <ImagePlus className="absolute top-2 right-2" size={24} />
+          <Input
+            className="w-9 h-9 absolute right-0 opacity-0"
+            type="file"
+            accept="image/*"
+          />
+          <Textarea
+            className="border-none resize-none min-h-9 max-h-9 pr-10"
+            placeholder="What's on your mind?"
+          />
+        </div>
+        <Button className="rounded-xl">Share Post</Button>
+      </div>
+    </div>
+  );
+}
+
+export default CreatePost;
