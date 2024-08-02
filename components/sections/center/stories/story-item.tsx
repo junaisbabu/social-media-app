7f195a04522f3e17de4aff1455ee45e418e2d776
Junais Babu <junaisdvs@gmail.com>
2024-08-02 21:56:18 +0530

    create story list ui

---
+++ components/sections/center/stories/story-item.tsx
@@ -0,0 +1,21 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
+import React from "react";
+import Story from "@/public/assets/story.jpg";
+
+function StoryItem() {
+  return (
+    <div className="rounded-xl w-32 h-56 overflow-hidden relative">
+      <Image src={Story} alt="Story" fill />
+      <Avatar className="h-6 w-6 rounded-lg absolute top-5 left-5 outline outline-2 outline-white outline-offset-2">
+        <AvatarImage src="https://github.com/shadcn.png" alt="story" />
+      </Avatar>
+
+      <span className="font-medium absolute -translate-x-1/2 left-1/2 bottom-4 text-white">
+        Sam Brown
+      </span>
+    </div>
+  );
+}
+
+export default StoryItem;
---
+++ components/sections/center/stories/story-list.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import StoryItem from "./story-item";
+
+function Stories() {
+  return (
+    <div className="rounded-xl overflow-hidden">
+      <ul className="flex gap-3 items-center flex-nowrap overflow-y-hidden overflow-x-auto no-scrollbar">
+        <li>
+          <StoryItem />
+        </li>
+        <li>
+          <StoryItem />
+        </li>
+        <li>
+          <StoryItem />
+        </li>
+        <li>
+          <StoryItem />
+        </li>
+        <li>
+          <StoryItem />
+        </li>
+        <li>
+          <StoryItem />
+        </li>
+        <li>
+          <StoryItem />
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Stories;
