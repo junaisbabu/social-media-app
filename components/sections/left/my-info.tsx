53c299e0fe9bac0c4dca912f34ba6ee0d9a01900
Junais Babu <junaisdvs@gmail.com>
2024-08-03 21:47:27 +0530

    create my-info ui component

---
+++ components/sections/left/my-info.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useAuthStore } from "@/components/auth/auth-state";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Card, CardContent } from "@/components/ui/card";
+import React from "react";
+
+function MyInfo() {
+  const { user } = useAuthStore();
+
+  return (
+    <Card className="overflow-hidden">
+      <CardContent className="pt-4 bg-white">
+        <div className="flex items-center gap-3">
+          <Avatar className="w-9 h-9 rounded-xl">
+            <AvatarImage
+              src={user?.photoURL || "https://github.com/shadcn.png"}
+              alt={user?.displayName || "Anonymous"}
+            />
+          </Avatar>
+          <div className="flex flex-col">
+            <h1 className="font-medium">Sam Brown</h1>
+            <span className="text-[10px] text-zinc-400">@sambrown</span>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default MyInfo;
