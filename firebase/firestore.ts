89ee0d0adcfd4a9eabed0c0da4c2f60b42fa6a68
Junais Babu <junaisdvs@gmail.com>
2024-08-15 22:59:58 +0530

    refactor firestoreService and create addDoc, getDocRef, setDoc and updateDoc methods

--- firebase/firestore.ts
+++ firebase/firestore.ts
@@ -1,38 +1,49 @@
 import {
+  addDoc,
   collection,
   doc,
-  DocumentData,
   getDoc,
   getDocs,
-  QuerySnapshot,
+  setDoc,
+  updateDoc,
 } from "firebase/firestore";
 import { db } from "./firebase";
 
 class FirestoreService {
-  getDoc = async (col: string, docId: string) => {
-    try {
-      const docRef = doc(db, col, docId);
-      const docSnap = await getDoc(docRef);
+  addDoc = async <T extends { [key: string]: any }>(col: string, data: T) => {
+    const collectionRef = collection(db, col);
+    return await addDoc(collectionRef, data); // return: docRef
+  };
 
-      if (docSnap.exists()) {
-        return docSnap.data();
-      } else {
-        console.log("No such document!");
-      }
-    } catch (err) {
-      console.error("Error:", err);
-    }
-    return null;
+  getDocRef = (col: string, docId: string) => {
+    return doc(collection(db, col), docId); // return: docRef
+  };
+
+  getDoc = async (col: string, docId: string) => {
+    const docRef = this.getDocRef(col, docId);
+    return await getDoc(docRef); // return: docSnap
   };
 
   getDocs = async (col: string) => {
-    try {
-      const querySnapshot = await getDocs(collection(db, col));
-      return querySnapshot.docs.map((doc) => doc.data());
-    } catch (err) {
-      console.error("Error:", err);
-    }
-    return [];
+    return await getDocs(collection(db, col)); // return: querySnapshot
+  };
+
+  setDoc = async <T extends { [key: string]: any }>(
+    col: string,
+    docId: string,
+    data: T
+  ) => {
+    const docRef = this.getDocRef(col, docId);
+    return await setDoc(docRef, data);
+  };
+
+  updateDoc = async <T extends { [key: string]: any }>(
+    col: string,
+    docId: string,
+    data: T
+  ) => {
+    const docRef = this.getDocRef(col, docId);
+    return await updateDoc(docRef, data);
   };
 }
 
