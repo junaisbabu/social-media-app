f83c26f20df73bbcf0eeb86be0cc0b8320c592e3
Junais Babu <junaisdvs@gmail.com>
2024-08-15 23:14:41 +0530

    create getDownloadFileURL method in storage.ts

---
+++ firebase/storage.ts
@@ -0,0 +1,14 @@
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { storage } from "@/firebase/firebase";
+
+class StorageService {
+  getDownloadFileURL = async (file: File | null): Promise<string | null> => {
+    if (!file) return null;
+
+    const imageRef = ref(storage, `images/${file.name + "/" + new Date()}`);
+    const snapshot = await uploadBytes(imageRef, file);
+    return await getDownloadURL(snapshot.ref); // return: url
+  };
+}
+
+export const storageService = new StorageService();
