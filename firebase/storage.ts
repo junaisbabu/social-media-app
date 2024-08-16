477ee397e9f3fa949dbb6120f2e3baa31fdb4593
Junais Babu <junaisdvs@gmail.com>
2024-08-17 00:22:01 +0530

    add deleteFile method in storageService

--- firebase/storage.ts
+++ firebase/storage.ts
@@ -1,13 +1,24 @@
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytes,
+} from "firebase/storage";
 import { storage } from "@/firebase/firebase";
 
 class StorageService {
   getDownloadFileURL = async (file: File | null): Promise<string | null> => {
     if (!file) return null;
 
-    const imageRef = ref(storage, `posts/${file.name + "/" + new Date()}`);
+    const imageRef = ref(storage, `posts/${file.name}`);
     const snapshot = await uploadBytes(imageRef, file);
     return await getDownloadURL(snapshot.ref); // return: url
+  };
+
+  deleteFile = async (file: string) => {
+    const fileRef = ref(storage, file);
+
+    return await deleteObject(fileRef);
   };
 }
 
