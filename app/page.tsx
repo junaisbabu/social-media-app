6f467269a2bbd361fe567a409c34fafefe0e9ceb
Junais Babu <junaisdvs@gmail.com>
2024-08-26 18:28:11 +0530

    add center section in root page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-"use client";
-
-import { useAuthStore } from "@/components/auth/auth-state";
-import { Button } from "@/components/ui/button";
-import { signOut } from "firebase/auth";
-import { auth } from "@/firebase";
+import CenterSection from "@/components/sections/center/center";
 
 export default function Home() {
-  const { user } = useAuthStore();
-
-  return (
-    <main className="min-h-screen">
-      User ID : {user?.uid}
-      <Button onClick={() => signOut(auth)}>Sign Out</Button>
-    </main>
-  );
+  return <CenterSection />;
 }
