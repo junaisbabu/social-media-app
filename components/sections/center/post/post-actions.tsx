d02c2b1ab3d36e77d0bd58fccec4ec031af56f5f
Junais Babu <junaisdvs@gmail.com>
2024-08-28 17:37:39 +0530

    create PostActions component separately and import it

---
+++ components/sections/center/post/post-actions.tsx
@@ -0,0 +1,140 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Bookmark, BookmarkX, Ellipsis, Pencil, Trash2 } from "lucide-react";
+import { useAuthStore } from "@/components/auth/auth-state";
+import { PostType } from "@/type";
+import { EditPost } from "./edit-post";
+import { DialogTrigger } from "@/components/ui/dialog";
+import { firestoreService } from "@/firebase/firestore";
+import { Collections } from "@/firebase/collections";
+import { arrayRemove, arrayUnion } from "firebase/firestore";
+import { deleteStorageFile } from "@/utils/delete-storage-file";
+import { useShowToast } from "@/hooks/useShowToast";
+
+function PostActions({ post }: { post: PostType }) {
+  const { user } = useAuthStore();
+  const [isSaved, setIsSaved] = useState<boolean>(false);
+  const { showSuccessToast, showErrorToast } = useShowToast();
+
+  const { uid, docId, file } = post;
+
+  useEffect(() => {
+    isSavedPost();
+  }, []);
+
+  if (!user?.uid) return null;
+
+  const isSavedPost = async () => {
+    try {
+      const savedPost = await firestoreService.getDoc(
+        Collections.SAVED_POSTS,
+        user.uid
+      );
+
+      if (savedPost.exists()) {
+        setIsSaved(savedPost.data().posts.includes(post.docId));
+      }
+    } catch (error) {
+      showErrorToast("isSavedPost: Error checking saved post status:" + error);
+    }
+  };
+
+  const savePost = async () => {
+    try {
+      const savedPost = await firestoreService.getDoc(
+        Collections.SAVED_POSTS,
+        user.uid
+      );
+
+      if (savedPost.exists()) {
+        if (isSaved) {
+          await firestoreService.updateDoc(Collections.SAVED_POSTS, user.uid, {
+            posts: arrayRemove(docId),
+          });
+        } else {
+          await firestoreService.updateDoc(Collections.SAVED_POSTS, user.uid, {
+            posts: arrayUnion(docId),
+          });
+        }
+      } else {
+        await firestoreService.setDoc(Collections.SAVED_POSTS, user.uid, {
+          posts: [docId],
+        });
+      }
+
+      isSavedPost();
+      showSuccessToast(
+        `Successfully ${isSaved ? "unsaved" : "saved"} post: ${docId}`
+      );
+    } catch (error) {
+      showErrorToast("savePost: Error saving or unsaving post:" + error);
+    }
+  };
+
+  const deletePost = async () => {
+    try {
+      await firestoreService.deleteDoc(Collections.POSTS, docId);
+      await firestoreService.updateDoc(Collections.MY_POSTS, uid, {
+        posts: arrayRemove(docId),
+      });
+
+      await deleteStorageFile(file);
+
+      showSuccessToast("Successfully deleted post: " + docId);
+    } catch (error) {
+      showErrorToast("deletePost: Error deleting post:" + error);
+    }
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Ellipsis className="h-4 px-1 border rounded text-zinc-400" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {user?.uid === uid && (
+          <div className="relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
+            <EditPost post={post}>
+              <DialogTrigger className="grid grid-cols-[12px_1fr] gap-1 items-center w-full place-items-start">
+                <Pencil size={12} /> Edit
+              </DialogTrigger>
+            </EditPost>
+          </div>
+        )}
+
+        <DropdownMenuItem
+          className="grid grid-cols-[12px_1fr] gap-1 items-center"
+          onClick={savePost}
+        >
+          {isSaved ? (
+            <>
+              <BookmarkX size={12} /> Unsave
+            </>
+          ) : (
+            <>
+              <Bookmark size={12} /> Save
+            </>
+          )}
+        </DropdownMenuItem>
+
+        {user?.uid === uid && (
+          <DropdownMenuItem
+            className="text-red-500 grid grid-cols-[12px_1fr] gap-1 items-center"
+            onClick={deletePost}
+          >
+            <Trash2 size={12} /> Delete
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default PostActions;
--- components/sections/center/post/post-card.tsx
+++ components/sections/center/post/post-card.tsx
@@ -7,23 +7,8 @@ import {
   CardFooter,
   CardHeader,
 } from "@/components/ui/card";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { PostType, UserType } from "@/type";
-import {
-  Bookmark,
-  BookmarkX,
-  Ellipsis,
-  Heart,
-  MessageCircle,
-  Pencil,
-  Share2,
-  Trash2,
-} from "lucide-react";
+import { Heart, MessageCircle, Share2 } from "lucide-react";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { firestoreService } from "@/firebase/firestore";
@@ -32,24 +17,20 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { arrayUnion, arrayRemove } from "firebase/firestore";
 import { useAuthStore } from "@/components/auth/auth-state";
-import { DialogTrigger } from "@/components/ui/dialog";
-import { EditPost } from "./edit-post";
-import { deleteStorageFile } from "@/utils/delete-storage-file";
 import { useShowToast } from "@/hooks/useShowToast";
+import PostActions from "./post-actions";
 
 dayjs.extend(relativeTime);
 
 function PostCard({ post }: { post: PostType }) {
   const { user } = useAuthStore();
   const [postedUser, setPostedUser] = useState<UserType>();
-  const [isSaved, setIsSaved] = useState<boolean>(false);
   const { uid, text, file, date, docId, likes } = post;
 
-  const { showSuccessToast, showErrorToast } = useShowToast();
+  const { showErrorToast } = useShowToast();
 
   useEffect(() => {
     getUser();
-    isSavedPost();
   }, []);
 
   if (!user?.uid) return null;
@@ -87,67 +68,6 @@ function PostCard({ post }: { post: PostType }) {
     }
   };
 
-  const isSavedPost = async () => {
-    try {
-      const savedPost = await firestoreService.getDoc(
-        Collections.SAVED_POSTS,
-        user.uid
-      );
-
-      if (savedPost.exists()) {
-        setIsSaved(savedPost.data().posts.includes(docId));
-      }
-    } catch (error) {
-      showErrorToast("isSavedPost: Error checking saved post status:" + error);
-    }
-  };
-
-  const savePost = async () => {
-    try {
-      const savedPost = await firestoreService.getDoc(
-        Collections.SAVED_POSTS,
-        user.uid
-      );
-
-      if (savedPost.exists()) {
-        if (isSaved) {
-          await firestoreService.updateDoc(Collections.SAVED_POSTS, user.uid, {
-            posts: arrayRemove(docId),
-          });
-        } else {
-          await firestoreService.updateDoc(Collections.SAVED_POSTS, user.uid, {
-            posts: arrayUnion(docId),
-          });
-        }
-      } else {
-        await firestoreService.setDoc(Collections.SAVED_POSTS, user.uid, {
-          posts: [docId],
-        });
-      }
-
-      showSuccessToast(
-        `Successfully ${isSaved ? "unsaved" : "saved"} post: ${docId}`
-      );
-    } catch (error) {
-      showErrorToast("savePost: Error saving or unsaving post:" + error);
-    }
-  };
-
-  const deletePost = async () => {
-    try {
-      await firestoreService.deleteDoc(Collections.POSTS, docId);
-      await firestoreService.updateDoc(Collections.MY_POSTS, uid, {
-        posts: arrayRemove(docId),
-      });
-
-      await deleteStorageFile(file);
-
-      showSuccessToast("Successfully deleted post: " + docId);
-    } catch (error) {
-      showErrorToast("deletePost: Error deleting post:" + error);
-    }
-  };
-
   return (
     <Card>
       <CardHeader>
@@ -163,46 +83,7 @@ function PostCard({ post }: { post: PostType }) {
               </span>
             </div>
           </div>
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Ellipsis className="h-4 px-1 border rounded text-zinc-400" />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              {user?.uid === uid && (
-                <div className="relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
-                  <EditPost post={post}>
-                    <DialogTrigger className="grid grid-cols-[12px_1fr] gap-1 items-center w-full place-items-start">
-                      <Pencil size={12} /> Edit
-                    </DialogTrigger>
-                  </EditPost>
-                </div>
-              )}
-
-              <DropdownMenuItem
-                className="grid grid-cols-[12px_1fr] gap-1 items-center"
-                onClick={savePost}
-              >
-                {isSaved ? (
-                  <>
-                    <BookmarkX size={12} /> Unsave
-                  </>
-                ) : (
-                  <>
-                    <Bookmark size={12} /> Save
-                  </>
-                )}
-              </DropdownMenuItem>
-
-              {user?.uid === uid && (
-                <DropdownMenuItem
-                  className="text-red-500 grid grid-cols-[12px_1fr] gap-1 items-center"
-                  onClick={deletePost}
-                >
-                  <Trash2 size={12} /> Delete
-                </DropdownMenuItem>
-              )}
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <PostActions post={post} />
         </div>
       </CardHeader>
       <CardContent className="p-6 pt-0">
