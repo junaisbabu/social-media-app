e272dcc8680510ba51bbd829ecd9fc49950866ee
Junais Babu <junaisdvs@gmail.com>
2024-09-11 22:06:37 +0530

    add handleSendFriendRequest to 'Add Friend' button in people-card.tsx

--- components/sections/center/people/people-card.tsx
+++ components/sections/center/people/people-card.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useAuthStore } from "@/components/auth/auth-state";
 import { Button } from "@/components/ui/button";
+import { useFriendRequest } from "@/hooks/use-friend-request";
 import { UserType } from "@/type";
 import { getUsername } from "@/utils/get-username";
 import Image from "next/image";
@@ -9,6 +11,12 @@ import React from "react";
 function PeopleCard({ person }: { person: UserType }) {
   const { uid, image, name } = person;
   const username = getUsername(name, uid);
+
+  const { isLoading, handleSendFriendRequest } = useFriendRequest();
+
+  const { user } = useAuthStore();
+
+  if (!user) return null;
 
   return (
     <div className="w-40 h-fit bg-white rounded-xl overflow-hidden p-4">
@@ -21,7 +29,13 @@ function PeopleCard({ person }: { person: UserType }) {
           <span className="mb-2 block text-gray-400">{username}</span>
         )}
       </div>
-      <Button className="block w-full">Add Friend</Button>
+      <Button
+        className="block w-full"
+        isLoading={isLoading}
+        onClick={() => handleSendFriendRequest(user.uid, uid)}
+      >
+        Add Friend
+      </Button>
     </div>
   );
 }
