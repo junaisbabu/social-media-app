bc23880c58269af69e50d88c70c9eb431cac95ce
Junais Babu <junaisdvs@gmail.com>
2024-09-12 23:28:12 +0530

    update useFriendRequest hook

--- hooks/use-friend-request.ts
+++ hooks/use-friend-request.ts
@@ -4,51 +4,23 @@ import { useState } from "react";
 import { useShowToast } from "./useShowToast";
 import { firestoreService } from "@/firebase/firestore";
 import { Collections } from "@/firebase/collections";
-import { arrayUnion } from "firebase/firestore";
 
 export const useFriendRequest = () => {
   const { showErrorToast } = useShowToast();
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleSendFriendRequest = async (
-    currentUserId: string,
-    newUserId: string
+    fromUserId: string,
+    toUserId: string
   ) => {
     setIsLoading(true);
     try {
-      const sendReqDocSnap = await firestoreService.getDoc(
-        Collections.FRIEND_REQUESTS,
-        currentUserId
-      );
-
-      if (sendReqDocSnap.exists()) {
-        firestoreService.updateDoc(Collections.FRIEND_REQUESTS, currentUserId, {
-          send: arrayUnion(newUserId),
-        });
-      } else {
-        await firestoreService.setDoc(
-          Collections.FRIEND_REQUESTS,
-          currentUserId,
-          {
-            send: arrayUnion(newUserId),
-          }
-        );
-      }
-
-      const receiveReqDocSnap = await firestoreService.getDoc(
-        Collections.FRIEND_REQUESTS,
-        newUserId
-      );
-
-      if (receiveReqDocSnap.exists()) {
-        firestoreService.updateDoc(Collections.FRIEND_REQUESTS, newUserId, {
-          receive: arrayUnion(currentUserId),
-        });
-      } else {
-        await firestoreService.setDoc(Collections.FRIEND_REQUESTS, newUserId, {
-          receive: arrayUnion(currentUserId),
-        });
-      }
+      firestoreService.addDoc(Collections.FRIEND_REQUESTS, {
+        from_user_id: fromUserId,
+        to_user_id: toUserId,
+        status: "pending",
+        timestamp: new Date().toISOString(),
+      });
     } catch (error) {
       showErrorToast("Error sending friend request: " + error);
     } finally {
