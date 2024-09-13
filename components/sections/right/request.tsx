ca5c19b959db634481b4a5b5b3ff6344147c187d
Junais Babu <junaisdvs@gmail.com>
2024-09-13 22:39:47 +0530

    integrate friend request in request.tsx

--- components/sections/right/request.tsx
+++ components/sections/right/request.tsx
@@ -1,27 +1,82 @@
+"use client";
+
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import React from "react";
+import { Collections } from "@/firebase/collections";
+import { firestoreService } from "@/firebase/firestore";
+import { useFriendRequest } from "@/hooks/use-friend-request";
+import { useShowToast } from "@/hooks/useShowToast";
+import { FriendRequestStatus, FriendRequestType, UserType } from "@/type";
+import React, { useEffect, useState } from "react";
 
-function Request() {
+function Request({ request }: { request: FriendRequestType }) {
+  const [requestUser, setRequestUser] = useState<UserType>();
+
+  const { isLoading, handleAcceptOrDeclineRequest } = useFriendRequest();
+
+  const { showErrorToast } = useShowToast();
+
+  const getUser = async () => {
+    try {
+      const userData = await firestoreService.getDoc(
+        Collections.USERS,
+        request.from_user_id
+      );
+      if (userData.exists()) {
+        setRequestUser(userData.data() as UserType);
+      } else {
+        showErrorToast(
+          "getUser: No data available for user with UID:" + request.from_user_id
+        );
+      }
+    } catch (error) {
+      showErrorToast("getUser: Error fetching user data:" + error);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <Card className="shrink-0">
       <CardContent className="p-6 bg-white space-y-4">
         <div className="flex items-center gap-3">
           <Avatar className="w-9 h-9 rounded-xl">
-            <AvatarImage
-              src={"https://github.com/shadcn.png"}
-              alt={"Anonymous"}
-            />
+            <AvatarImage src={requestUser?.image} alt={requestUser?.name} />
           </Avatar>
           <p className="text-[10px] text-zinc-400 flex gap-1 items-baseline">
-            <span className="font-medium text-base text-black">Ajmal Khan</span>
+            <span className="font-medium text-base text-black">
+              {requestUser?.name}
+            </span>
             wants to add you to friends
           </p>
         </div>
         <div className="flex gap-4">
-          <Button className="flex-1 rounded-xl">Accept</Button>
-          <Button className="flex-1 rounded-xl" variant="outline">
+          <Button
+            className="flex-1 rounded-xl"
+            isLoading={isLoading}
+            onClick={() =>
+              handleAcceptOrDeclineRequest(
+                request.doc_id,
+                FriendRequestStatus.ACCEPTED
+              )
+            }
+          >
+            Accept
+          </Button>
+          <Button
+            className="flex-1 rounded-xl"
+            variant="outline"
+            isLoading={isLoading}
+            onClick={() =>
+              handleAcceptOrDeclineRequest(
+                request.doc_id,
+                FriendRequestStatus.DECLINED
+              )
+            }
+          >
             Decline
           </Button>
         </div>
