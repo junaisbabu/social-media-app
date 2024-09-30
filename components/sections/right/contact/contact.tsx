62c7c9f128dcedabdf0ad0597763bfb835feefa8
Junais Babu <junaisdvs@gmail.com>
2024-09-30 21:17:29 +0530

    implement sendFriendRequest in contact.tsx

--- components/sections/right/contact/contact.tsx
+++ components/sections/right/contact/contact.tsx
@@ -6,6 +6,8 @@ import React from "react";
 import ContactActions from "./contact-actions";
 import { UserRoundPlus } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useFriendRequest } from "@/hooks/use-friend-request";
+import { useAuthStore } from "@/components/auth/auth-state";
 
 function Contact({
   contact,
@@ -14,6 +16,9 @@ function Contact({
   contact: UserType;
   isFriend: boolean;
 }) {
+  const { user } = useAuthStore();
+  const { isLoading, handleSendFriendRequest } = useFriendRequest();
+
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-3">
@@ -25,7 +30,13 @@ function Contact({
       {isFriend ? (
         <ContactActions />
       ) : (
-        <Button className="p-0 m-0 h-8 w-8 rounded-sm">
+        <Button
+          className="p-0 m-0 h-8 w-8 rounded-sm"
+          isLoading={isLoading}
+          onClick={() => {
+            if (user) handleSendFriendRequest(user.uid, contact.uid);
+          }}
+        >
           <UserRoundPlus strokeWidth={1.8} size={20} />
         </Button>
       )}
