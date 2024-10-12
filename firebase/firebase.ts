588f090cf79d3fd18151ebe0b46b29d2ab45aa79
Junais Babu <junaisdvs@gmail.com>
2024-10-12 18:24:49 +0530

    remove firebase analytics to prevent the build error

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -1,16 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDzEnP7nUoKNRhTEXSOommmLrq2jjk2EfA",
   authDomain: "social-media-app-61942.firebaseapp.com",
@@ -27,8 +20,6 @@ const auth = getAuth(app);
 auth.useDeviceLanguage();
 
 const db = getFirestore(app);
-
-const analytics = getAnalytics(app);
 
 const googleAuthProvider = new GoogleAuthProvider();
 
