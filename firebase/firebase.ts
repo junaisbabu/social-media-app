442299710d3750534c0e523a80ce5d61f662a4b0
Junais Babu <junaisdvs@gmail.com>
2024-10-17 18:03:17 +0530

    add env variables

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzEnP7nUoKNRhTEXSOommmLrq2jjk2EfA",
-  authDomain: "social-media-app-61942.firebaseapp.com",
-  projectId: "social-media-app-61942",
-  storageBucket: "social-media-app-61942.appspot.com",
-  messagingSenderId: "541606342388",
-  appId: "1:541606342388:web:17e2351f27af60770db8a1",
-  measurementId: "G-ETEPV7NJEV",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
