5ec73cf059e775815d23e2064751d325a36738a9
Junais Babu <junaisdvs@gmail.com>
2024-10-18 15:53:24 +0530

    fix auth domain error

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -6,7 +6,7 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  authDomain: "social-media-app-61942.firebaseapp.com",
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
