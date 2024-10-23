e653089a249c88c96a3fa992ac7dde9662110f00
Junais Babu <junaisdvs@gmail.com>
2024-10-23 21:11:37 +0530

    add title to my posts page

--- app/(root)/my-posts/page.tsx
+++ app/(root)/my-posts/page.tsx
@@ -3,7 +3,8 @@ import React from "react";
 
 function Page() {
   return (
-    <div className="h-full w-full flex-1 rounded-xl overflow-y-auto flex flex-col flex-nowrap space-y-6 pb-14 no-scrollbar">
+    <div className="h-full w-full flex-1 rounded-xl overflow-y-auto flex flex-col flex-nowrap space-y-4 pb-14 no-scrollbar">
+      <h1 className="font-medium text-lg">My Posts</h1>
       <MyPostList />
     </div>
   );
