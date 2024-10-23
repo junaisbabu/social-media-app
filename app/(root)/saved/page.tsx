49dcb0ccdec5fda32f9999fea1df53a6f600298c
Junais Babu <junaisdvs@gmail.com>
2024-10-23 21:12:55 +0530

    add title to saved posts page

--- app/(root)/saved/page.tsx
+++ app/(root)/saved/page.tsx
@@ -2,7 +2,8 @@ import SavedList from "@/components/sections/center/post/saved-list";
 
 export default function Saved() {
   return (
-    <div className="h-full w-full flex-1 rounded-xl overflow-y-auto flex flex-col flex-nowrap space-y-6 pb-14 no-scrollbar">
+    <div className="h-full w-full flex-1 rounded-xl overflow-y-auto flex flex-col flex-nowrap space-y-4 pb-14 no-scrollbar">
+      <h1 className="font-medium text-lg">Saved Posts</h1>
       <SavedList />
     </div>
   );
